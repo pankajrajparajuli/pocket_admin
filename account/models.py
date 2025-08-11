423aedc8c3a27669e7ac43be8ae2abce7e982697
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-12 00:30:00 +0545

    created model for user

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class userProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
