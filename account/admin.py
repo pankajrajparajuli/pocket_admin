f2bcba2774074709d1003dad8574ec249f51260b
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-08-12 23:11:44 +0545

    added models to admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import userProfile
 # Register your models here.
+@admin.register(userProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'profile_picture', 'first_name', 'last_name', 'email')
+    search_fields = ('user__username', 'email')
+    list_filter = ('user__is_active', 'user__is_staff')
+    ordering = ('user__username',)
